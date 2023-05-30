041ad6dac1e9b627e4fb935ce7a50d8b5bdd7fec
Roberto Hasa <hasa.roberto@live.it>
2023-05-30 12:56:12 +0200

    finished projects pages

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 
 // Funzione per aprire l'immagine a schermo intero
 function openFullscreenImg() {
-    var img = document.getElementById('myImg');
-    var fullscreenImg = document.getElementById('fullscreenImg');
+    let img = document.getElementById('myImg');
+    let fullscreenImg = document.getElementById('fullscreenImg');
 
     fullscreenImg.style.display = 'block'; // Mostra l'immagine a schermo intero
     fullscreenImg.innerHTML = "<img src='" + img.src + "'>"; // Inserisce l'immagine nell'elemento fullscreenImg
@@ -11,7 +11,15 @@ function openFullscreenImg() {
 
 // Funzione per chiudere l'immagine a schermo intero
 function closeFullscreenImg() {
-    var fullscreenImg = document.getElementById('fullscreenImg');
+    let fullscreenImg = document.getElementById('fullscreenImg');
     fullscreenImg.style.display = 'none'; // Nasconde l'immagine a schermo intero
     fullscreenImg.innerHTML = ""; // Rimuove l'immagine dall'elemento fullscreenImg
+}
+
+function addAnimation() {
+    let button = document.querySelector('#cv-downloader');
+    button.classList.remove('animate__rubberBand');
+    setTimeout(function () {
+        button.classList.add('animate__rubberBand');
+    }, 10);
 }
